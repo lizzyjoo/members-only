0bc91bac5260eef2d35fc8fe6d09ffc864e862dd
lizzyjoo <jejviolinist@gmail.com>
2025-07-09 21:24:12 -0400

    build command

--- config/database.js
+++ config/database.js
@@ -4,7 +4,11 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString:
+    process.env.DATABASE_URL || "postgresql://localhost/members_only",
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : false,
 });
-
 module.exports = pool;
